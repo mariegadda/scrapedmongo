d89a085a13e1e66d564b9ec5eba980538e7f3d1d
Marie Gadda <mariegadda@gmail.com>
2017-07-02 12:53:38 -0400

    index.handlebars rendering

--- controllers/routes.js
+++ controllers/routes.js
@@ -15,62 +15,60 @@ mongoose.Promise = Promise;
 module.exports = function(app) {
 
         app.get("/", function(req, res) {
-            console.log("what up");
-          Tracks.find(function(error, doc){
-            if (error) {
+           Tracks.find(function(error, doc){
+            if (error){
                 console.log(error);
+            }else{
+                console.log("here's the doc" + doc);
+                var hndlObject = doc;
+                 res.render("../views/index.handlebars", {hndlObject});
             }
-            else{
-
-                console.log("this is the doc" + doc);
-                var Scrape = doc;
-                res.render("index", {Scrape});
-            }
-          });
+           });
+     
         });
 
 
-        app.get("/scrape", function(req, res) {
-            // First, we grab the body of the html with request
-            request("http://www.pitchfork.com/reviews/best/tracks/", function(error, response, html) {
+    //     app.get("/scrape", function(req, res) {
+    //         // First, we grab the body of the html with request
+    //         request("http://www.pitchfork.com/reviews/best/tracks/", function(error, response, html) {
 
-                // Then, we load that into cheerio and save it to $ for a shorthand selector
-                var $ = cheerio.load(html);
+    //             // Then, we load that into cheerio and save it to $ for a shorthand selector
+    //             var $ = cheerio.load(html);
 
-                // Save an empty result object
-                var result = {};
-                $('ul.artist-list').each(function(i, element) {
+    //             // Save an empty result object
+    //             var result = {};
+    //             $('ul.artist-list').each(function(i, element) {
 
-                    result.artist = $(element).children().text();
-                    result.title = $(element).siblings().text();
-                    //empty area for the notes
-                    // result.image = $(element).parents(".track-collection-item").children().attr("src");
-                    result.note = [];
+    //                 result.artist = $(element).children().text();
+    //                 result.title = $(element).siblings().text();
+    //                 //empty area for the notes
+    //                 // result.image = $(element).parents(".track-collection-item").children().attr("src");
+    //                 result.note = [];
 
-                    var entry = new Tracks(result);
+    //                 var entry = new Tracks(result);
 
-                    // save that entry to the db
-                    entry.save(function(err, doc) {
-                        if (err) {
-                            console.log(err);
-                        } else {
-                            console.log("Got in " + doc);
-                        }
-                    });
-                });
-            });
-            Tracks.find({})
-                .populate("note")
-                .exec(function(error, doc) {
-                    // Log any errors
-                    if (error) {
-                        console.log(error);
-                    }
-                    // Or send the doc to the browser as a json object
-                    else {
-                        console.log(doc);
-                        res.json(doc);
-                    }
-                });
-        });
+    //                 // save that entry to the db
+    //                 entry.save(function(err, doc) {
+    //                     if (err) {
+    //                         console.log(err);
+    //                     } else {
+    //                         console.log("Got in " + doc);
+    //                     }
+    //                 });
+    //             });
+    //         });
+    //         Tracks.find({})
+    //             .populate("note")
+    //             .exec(function(error, doc) {
+    //                 // Log any errors
+    //                 if (error) {
+    //                     console.log(error);
+    //                 }
+    //                 // Or send the doc to the browser as a json object
+    //                 else {
+    //                     console.log(doc);
+    //                     res.json(doc);
+    //                 }
+    //             });
+    //     });
     };
